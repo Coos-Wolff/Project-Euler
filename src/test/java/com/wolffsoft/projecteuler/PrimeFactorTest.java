f1614ea19fce833a4685a789d545528fe85607f1
Coos-Wolff <coos.wolff@outlook.com>
2018-04-01 21:36:45 +0200

    Refactored PrimeFactor test class

--- src/test/java/com/wolffsoft/projecteuler/PrimeFactorTest.java
+++ src/test/java/com/wolffsoft/projecteuler/PrimeFactorTest.java
@@ -2,71 +2,15 @@ package com.wolffsoft.projecteuler;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class PrimeFactorTest {
 
+    private PrimeFactors primeFactors = new PrimeFactors();
+
     @Test
     public void shouldTestPrimeFactors() {
-        assertEquals(29, isLargestPrimeFactor(13195));
-    }
-
-    public int isLargestPrimeFactor(int number) {
-        ArrayList<Integer> primes = new ArrayList<>();
-        int i =0;
-        int num =0;
-        //Empty String
-        String  primeNumbers = "";
-
-        for (i = 1; i <= number; i++)
-        {
-            int counter=0;
-            for(num =i; num>=1; num--)
-            {
-                if(i%num==0)
-                {
-                    counter = counter + 1;
-                }
-            }
-            if (counter ==2)
-            {
-                //Appended the Prime number to the String
-                primeNumbers = primeNumbers + i ;
-                primes.add(i);
-            }
-
-        }
-        for (int prime : primes) {
-            
-        }
-        System.out.println(primes.toString());
-        return (Integer.valueOf(primeNumbers));
-    }
-
-    private List<Integer> primeNumbers(int number) {
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 2; i < number; i++) {
-            if (isPrime(i)) {
-                primes.add(i);
-            }
-        }
-        System.out.println(primes);
-        return primes;
-    }
-
-    private boolean isPrime(int number) {
-        boolean isPrime = false;
-        for (int i = 2; 2 * i < number; i++) {
-            if (number % i == 0) {
-                isPrime = false;
-            } else {
-                isPrime = true;
-            }
-        }
-        return isPrime;
+        assertEquals(29, primeFactors.getLargestPrimeFactor(13195L));
+        assertEquals(6857, primeFactors.getLargestPrimeFactor(600851475143L));
     }
 }
